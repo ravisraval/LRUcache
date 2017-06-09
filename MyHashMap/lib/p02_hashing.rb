7fe3c02611c96479e61ff77cb8fa6a91c15e9a0e
Isak Mladenoff <imladenoff@gmail.com>
2017-06-09 11:28:06 -0700

    Write hash methods

--- MyHashMap/lib/p02_hashing.rb
+++ MyHashMap/lib/p02_hashing.rb
@@ -2,13 +2,28 @@ class Fixnum
   # Fixnum#hash already implemented for you
 end
 
+# class Array
+#   def hash
+#     result = {}
+#     self.each_with_index do |el, idx|
+#       result[idx] = el.ord.hash
+#     end
+#     result
+#   end
+# end
+
 class Array
-  def hash
+  def hash #[1,2,4]
+    each_with_index.inject(0) do |intermediate_hash, (el, i)|
+      (el.hash + i.hash) ^ intermediate_hash
+    end
   end
 end
 
+
 class String
   def hash
+    chars.map(&:ord).hash
   end
 end
 
@@ -16,6 +31,14 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    to_a.sort_by(&:hash).hash
   end
 end
+
+=begin
+[1,2]
+
+0 1
+1 2
+
+=end
