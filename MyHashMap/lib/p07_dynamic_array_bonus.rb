18dec4efc577aca06f33ab36280b733477b82029
Isak Mladenoff <imladenoff@gmail.com>
2017-06-09 17:03:51 -0700

    Progress on dynamic arrays

--- MyHashMap/lib/p07_dynamic_array_bonus.rb
+++ MyHashMap/lib/p07_dynamic_array_bonus.rb
@@ -25,17 +25,30 @@ class StaticArray
 end
 
 class DynamicArray
+  include Enumerable
   attr_reader :count
 
   def initialize(capacity = 8)
-    @store = StaticArray.new(capacity)
+    @store = StaticArray.new(capacity) {nil}
     @count = 0
   end
 
   def [](i)
+    until i >= capacity
+      resize!
+    end
+    if i > -1
+      @store[i]
+    else
+      @store[length + i]
+    end
   end
 
   def []=(i, val)
+    until i >= capacity
+      resize!
+    end
+    @store[i] = val
   end
 
   def capacity
@@ -43,27 +56,72 @@ class DynamicArray
   end
 
   def include?(val)
+    each do |el|
+      return true if el == val
+    end
+    false
   end
 
   def push(val)
+    @count += 1
+    resize! if @count > capacity
+    @store[count - 1] = val
   end
 
   def unshift(val)
+    @count += 1
+    resize! if @count > capacity
+    i = @count
+    while i > 0
+      @store[i] = @store[i - 1]
+      i -= 1
+    end
+    @store[0] = val
+    @store
   end
 
   def pop
+    @store[count - 1] = nil
+    @count -= 1
   end
 
   def shift
+    i = 0
+    while i < @count
+      @store[i] = @store[i + 1]
+      i += 1
+    end
+    pop
   end
 
   def first
+    @store[0]
   end
 
   def last
+    @store[count - 1]
   end
 
   def each
+    i = 0
+    while i < count
+      yield(@store[i])
+      i += 1
+    end
+    @store
+  end
+
+  def each_with_index
+    i = 0
+    while i < count
+      yield(@store[i], i)
+      i += 1
+    end
+    @store
+  end
+
+  def self
+    to_s
   end
 
   def to_s
@@ -81,5 +139,10 @@ class DynamicArray
   private
 
   def resize!
+    new_store = StaticArray.new(capacity * 2)
+    each_with_index do |el, i|
+      new_store[i] = el
+    end
+    @store = new_store
   end
 end
